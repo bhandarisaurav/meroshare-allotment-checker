19d5a525498e23a2e8e2c68d06740bfbc02e67f2
bhandarisaurav <sbhandari@deerhold.org>
2021-02-20 23:52:06 +0545

    :zap: Removed debug status

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from script import get_allotted_stock_from_boid
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
-app.run(debug=True)
-
 
 @app.route('/')
 def main():
