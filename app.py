db53e646e8f48f69d63c4fe058297f132ab3ffbd
bhandarisaurav <sbhandari@deerhold.org>
2021-02-21 09:30:17 +0545

    :hammer: Added Environment configuration

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from script import get_allotted_stock_from_boid
 from flask import Flask, render_template, jsonify, request
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
 def main():
@@ -12,3 +16,7 @@ def allotment_check():
     boid_list = request.args.get('boid').split(",")
     print(boid_list)
     return jsonify(get_allotted_stock_from_boid(boid_list))
+
+
+if __name__ == '__main__':
+    app.run()
--- script.py
+++ script.py
@@ -1,5 +1,10 @@
+import os
 import time
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+firefox_options = Options()
+firefox_options.headless = True
 
 GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
@@ -12,7 +17,12 @@ chrome_options.binary_location = GOOGLE_CHROME_BIN
 
 def get_allotted_stock_from_boid(boid_list=['1301090000394531', '1301090000382579']):
     response = {}
-    driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    driver = None
+    if(os.environ['APP_SETTINGS'] == "config.DevelopmentConfig"):
+        driver = webdriver.Firefox(options=firefox_options)
+    else:
+        driver = webdriver.Chrome(
+            CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver.get("https://www.sharesansar.com/ipo-result")
     boid_element = driver.find_element_by_id("boid")
     for boid in boid_list:
