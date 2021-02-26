82f9d1301b1da93ed043a91d2ba6e3ed8f9a084d
bhandarisaurav <sbhandari@deerhold.org>
2021-02-26 11:18:25 +0545

    Rendered Result in table

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from script import get_allotted_stock_from_boid
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
@@ -8,14 +8,15 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def main():
-    return render_template('template.html')
+    return render_template('index.html')
 
 
 @app.route('/allotment-check')
 def allotment_check():
     boid_list = request.args.get('boid').split(",")
     print(boid_list)
-    return jsonify(get_allotted_stock_from_boid(boid_list))
+    return render_template('result.html', data=get_allotted_stock_from_boid(boid_list))
+    # return jsonify(get_allotted_stock_from_boid(boid_list))
 
 
 if __name__ == '__main__':
